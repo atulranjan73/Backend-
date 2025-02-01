5f3485a786d024a2c758b30f5b9fea6c44863aed
AtulRanjan <atulranjan164@gmail.com>
2025-02-01 15:19:29 +0530

    add model user and video

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -41,16 +41,43 @@ const userSchema = new Schema({
 
 });
 
-
-userSchema.pre('save' , async function (next) {
-    if(!this.isModified("password")) return next();
-
-    this.password = bcrypt.hash(this.password , 8 )
-    next()
-})
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password , this.password)
-}
-
-module.exports = mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+  });
+  
+  // Compare hashed password
+  userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  
+  // Generate JWT Access Token
+  userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+      { _id: this._id },
+      process.env.ACCESS_TOKEN_SECRET || "your_secret_key",
+      { expiresIn:process.env.ACCESS_TOKEN_EXPIRE }
+    );
+  };
+  
+  // Generate JWT Refresh Token
+  userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+      { _id: this._id },
+      process.env.REFRESH_TOKEN_SECRET || "your_refresh_secret_key",
+      { expiresIn:  process.env.REFRESH_TOKEN_EXPIRE}
+    );
+  };
+  
+  // Hide password when converting to JSON
+  userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+    return user;
+  };
+  
+  userSchema.plugin(mongooseAggregatePaginate);
+  
+  module.exports = mongoose.model("User", userSchema);
